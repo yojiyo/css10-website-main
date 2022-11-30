856c897866dfd3a1decfcba3bb63a0ad80b8fa4c
jyooah <117620661+jyooah@users.noreply.github.com>
2022-11-30 15:12:57 +0800

    v1.0.2

--- index.js
+++ index.js
@@ -34,17 +34,3 @@ const handleRightClick = () => {
   });
 }
 
-const nav = document.querySelector("nav");
-
-const handleNavToggle = () => {  
-  nav.dataset.transitionable = "true";
-  
-  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
-}
-
-window.matchMedia("(max-width: 800px)").onchange = e => {
-  nav.dataset.transitionable = "false";
-
-  nav.dataset.toggled = "false";
-};
-
---
+++ indexmobile.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
